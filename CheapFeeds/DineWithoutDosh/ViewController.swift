f099d9242fac0882a0ca7870ccc6ef3648e6eced
balajesush <balajesush@ecs.vuw.ac.nz>
2017-11-07 11:43:44 +1300

    Refactored code in main VC, Fixed constraint issues

--- CheapFeeds/DineWithoutDosh/ViewController.swift
+++ CheapFeeds/DineWithoutDosh/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
     @IBOutlet weak var view2: UIView!
     
     var x = 0
+    var stopTheGeo = 0
 
     var pickCC = [String]()
 
@@ -29,7 +30,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
     var longs = CLLocationDegrees()
     var xx = 0
     var yy = 20
-    var pickedCuisine = "Any"
+    var pickedCuisine = "--- Any ---"
 
     var filteredAnyByCost = [RestaurantData]()
     var restaurantInfo = [RestaurantData]()
@@ -90,12 +91,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
             longs = location.coordinate.longitude
             
             while xx < 100 {
-             self.uploadData()
+                self.uploadData(changingURL: "https://developers.zomato.com/api/v2.1/search?&lat=\(lats)&lon=\(longs)&start=\(xx)&count=\(yy)", APIHeader: "restaurants")
              xx += 20
              yy += 20
              }
-            self.uploadData1()
-
+            while stopTheGeo < 1 {
+                self.uploadData(changingURL: "https://developers.zomato.com/api/v2.1/geocode?lat=\(lats)&lon=\(longs)", APIHeader: "nearby_restaurants")
+                stopTheGeo += 1
+            }
         }
     }
     
@@ -133,7 +136,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
                     }
                 }
                 if(items.averageCostPP <= budget && items.averageCostPP > 0){
-                    if(pickedCuisine == "Any"){
+                    if(pickedCuisine == "--- Any ---"){
                         
                         let all = items
                         filteredAnyByCost.append(all)
@@ -154,7 +157,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
                     }
                 }
                 if(items.averageCostPP <= budget && items.averageCostPP > 0){
-                if(pickedCuisine == "Any"){
+                if(pickedCuisine == "--- Any ---"){
                     
                     let all = items
                     filteredAnyByCost.append(all)
@@ -165,7 +168,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-        
         
         if(segue.identifier == "push to results" ){
  
@@ -188,13 +190,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
             }
         }
         
-        
-        
         if(segue.identifier == "push to wheel" ){
             
         let DestViewController: RandomGenerator = segue.destination as! RandomGenerator
             
-            if(textf.text == "" && pickedCuisine == "Any"){
+            if(textf.text == "" && pickedCuisine == "--- Any ---"){
                 createAlert(title: "Warning", message: "Please enter your budget")
             }
             else{
@@ -204,7 +204,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
                 else{
                     createAlert(title: "Warning", message: "No results matching your budget/cuisine. Try increasing your budget!")
                 }
-                //DestViewController.dataToSelectFrom = filteredAnyByCost
                 filteredAnyByCost.removeAll()
             }
         }
@@ -226,13 +225,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
         // show the alert
         self.present(alert, animated: true, completion: nil)
     }
+    
 
-
-    func uploadData(){
+    func uploadData(changingURL: String, APIHeader: String){
         
-        let zomatoKey =
+        let zomatoKey = 
 
-        let urlString = "https://developers.zomato.com/api/v2.1/search?&lat=\(lats)&lon=\(longs)&start=\(xx)&count=\(yy)";
+        let urlString = changingURL
         
         let url = NSURL(string: urlString)
         
@@ -249,7 +248,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
                     if httpResponse?.statusCode == 200 {
                         do {
                             if let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
-                                if let restaurants = json["restaurants"] as? [NSDictionary] {
+                                if let restaurants = json[APIHeader] as? [NSDictionary] {
                                     for rest in restaurants {
                                         let restaurant = rest["restaurant"] as! NSDictionary
                                         let RestId = (restaurant["id"] as! String)
@@ -275,7 +274,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
                                         let sepCuis = cuisines.components(separatedBy: ", ")
                                         
                                         self.pickCC.append(sepCuis[0])
-                                        
+                                        self.pickCC.insert("--- Any ---", at: 0 )
                                         self.pickCC = Array(Set(self.pickCC))
                                         
                                         self.pickCC = self.pickCC.sorted() { $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
@@ -286,95 +285,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
                                     OperationQueue.main.addOperation {
                                         
                                         self.cuisinePicker.reloadAllComponents()
+                                        
                                     }
+                                    
                                 }
-                            }
-                        }
-                        catch {
-                            print(error)
-                        }
-                    }
-                }
-            })
-            task.resume()
-    }
-}
-    
-    func uploadData1(){
-        
-        let zomatoKey =
-
-        let urlString = "https://developers.zomato.com/api/v2.1/geocode?lat=\(lats)&lon=\(longs)"
-        
-        let url = NSURL(string: urlString)
-        
-        if url != nil {
-            let request = NSMutableURLRequest(url: url! as URL)
-            request.httpMethod = "GET"
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            request.addValue(zomatoKey, forHTTPHeaderField: "user_key")
-            
-        
-            let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {(data, response, error) -> Void in
-                if error == nil {
-                    let httpResponse = response as! HTTPURLResponse!
-                  
-                    if httpResponse?.statusCode == 200 {
-                        do {
-                      
-                            if let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
                                 
-                                if let restaurants = json["nearby_restaurants"] as? [NSDictionary] {
-                                    for rest in restaurants {
-                                        self.x += 1
-                                        //print(self.x)
-                                        let restaurant = rest["restaurant"] as! NSDictionary
-                                        let RestId = (restaurant["id"] as! String)
-                                        let averageCostPP = (restaurant["average_cost_for_two"] as! Int/2)
-                                        let currency = (restaurant["currency"] as! String)
-                                        let mainImage = (restaurant["featured_image"] as? String)
-                                        let cuisines = (restaurant["cuisines"] as! String)
-                                        let restURL = (restaurant["url"] as! String)
-                                        let location = restaurant["location"] as! NSDictionary
-                                        let restAddress = (location["address"] as! String)
-                                        let restCity = (location["city"] as! String)
-                                        let latitude = (location["latitude"] as! String)
-                                        let longitude = (location["longitude"] as! String)
-                                        let menuLink = (restaurant["menu_url"] as! String)
-                                        let name = (restaurant["name"] as! String )
-                                        let phone = (restaurant["phone_numbers"] as? String)
-                                        
-                                        let user_rating = restaurant["user_rating"] as! NSDictionary
-                                        let rating = (user_rating["aggregate_rating"] as! String)
-                                        
-                                        let populate = RestaurantData(id: RestId, averageCostPP: averageCostPP, currency: currency, mainImage: mainImage, cuisines: cuisines, url: restURL, address: restAddress, city: restCity, latitude: latitude, longitude: longitude, menuUrl: menuLink, name: name, aggregateRating: rating, phoneNumber: phone)
-                                        
-                                        let sepCuis = cuisines.components(separatedBy: ", ")
-                                        
-                                        self.pickCC.append(sepCuis[0])
-                                        
-                                        self.pickCC = Array(Set(self.pickCC))
-                                        
-                                        self.pickCC = self.pickCC.sorted() { $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending }
-                                        
-                                        self.restaurantInfo.append(populate)
-                                        
-                                    }
-                                    OperationQueue.main.addOperation {
-                                        self.cuisinePicker.reloadAllComponents()
-                                        
-                                        self.pickCC.insert("Any", at: 0 )
-                                    }
-                                }
                             }
+                           
                         }
                         catch {
                             print(error)
                         }
                     }
+                    
                 }
+                
             })
             task.resume()
         }
+        
     }
+   
 }
