a0dc068d4ee9976d588a91888e41fba28b2dbfa7
balajesush <balajesush@ecs.vuw.ac.nz>
2017-11-15 21:11:29 +1300

    Changes for iPad

--- CheapFeeds/DineWithoutDosh/ViewController.swift
+++ CheapFeeds/DineWithoutDosh/ViewController.swift
@@ -254,7 +254,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UIPickerViewD
     /* API call and initialisation */
     func uploadData(changingURL: String, APIHeader: String){
         
-        let zomatoKey = 
+        let zomatoKey = "d7aa15e105531ac1ece595a45666a3e8"
 
         let urlString = changingURL
         
