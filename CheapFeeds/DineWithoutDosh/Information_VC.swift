e852a4783646f719d5a5c70c81a362d6367ea737
balajesush <balajesush@ecs.vuw.ac.nz>
2017-12-06 22:32:05 +1300

    updated info page

--- CheapFeeds/DineWithoutDosh/Information_VC.swift
+++ CheapFeeds/DineWithoutDosh/Information_VC.swift
@@ -10,17 +10,15 @@ import UIKit
 
 class Information_VC: UIViewController {
 
-    @IBOutlet weak var view1: UIView!
-    @IBOutlet weak var mainText: UILabel!
     @IBOutlet weak var mainTitle: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view1.layer.shadowColor = UIColor.darkGray.cgColor
+        /*view1.layer.shadowColor = UIColor.darkGray.cgColor
         view1.layer.shadowOpacity = 1
         view1.layer.shadowOffset = CGSize.zero
-        view1.layer.shadowRadius = 1
+        view1.layer.shadowRadius = 1*/
 
     }
 
